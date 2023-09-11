f5461b17edbbebd6e04f3010e045b3b94b7d824c
Ollie Paradise <ollieparadise@yahoo.co.uk>
2023-09-11 11:35:21 +0100

    Initial build

---
+++ Position.cs
@@ -0,0 +1,14 @@
+﻿namespace Tetris
+{
+    public class Position
+    {
+        public int Row { get; set; }
+        public int Column { get; set; } 
+
+        public Position(int x, int y)
+        {
+            Row = x;
+            Column = y;
+        }
+    }
+}
